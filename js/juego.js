db3bcd6ece3e0b66597a1eb88ca7f3392c9493f5
priscilaGarciaMoya <priscilagarciamoya@alu.frt.utn.edu.ar>
2023-05-07 15:33:14 -0300

    se agrega modificacion del challenfe 37 de js/DOM

--- js/juego.js
+++ js/juego.js
@@ -4,25 +4,33 @@ function crearNumeroRandom() {
     numeroRandom = generarNumero(-100, 100);
     document.getElementById("inputUsuario").removeAttribute("readonly", false);
     document.getElementById("btnEnviarUsuario").removeAttribute("disabled", false);
-    document.getElementById("btnLimpiarUsuario").removeAttribute("disabled", false);
     document.getElementById("btnInciarJuego").setAttribute("disabled", true);
+    console.log(numeroRandom);
 }
 function obtenerAdivinaza() {
     let numero = parseInt(document.getElementById("inputUsuario").value);
     let contenedorPadreRespuesta = document.getElementById("panelRespuesta");
     let h2 = document.createElement("h2");
-
-    if (numero == numeroRandom) {
-        h2.innerHTML = '<i class="bi bi-stars"></i> Usted Adivino el Numero';
-        return contenedorPadreRespuesta.appendChild(h2);
-    }
-    if (numero < numeroRandom) {
-        h2.innerHTML = '<i class="bi bi-x-square-fill"></i> Usted No Adivino el Numero!. Adivino un numero menor que el Número Mágico';
-       return contenedorPadreRespuesta.appendChild(h2);
-    }
-    if (numero > numeroRandom) {
-        h2.innerHTML = '<i class="bi bi-x-square-fill"></i> Usted No Adivino el Numero!. Adivino un numero mayor que el Número Mágico';
-       return contenedorPadreRespuesta.appendChild(h2);
+    switch (true) {
+        case numero == numeroRandom:
+            h2.innerHTML = '<i class="bi bi-stars"></i> Usted Adivino el Numero';
+            contenedorPadreRespuesta.appendChild(h2);
+            setTimeout(empezarDeNuevo,3000);
+            break;
+        case numero < numeroRandom:
+            h2.innerHTML = '<i class="bi bi-x-square-fill"></i> Usted No Adivino el Numero!. Adivino un numero menor que el Número Mágico';
+            contenedorPadreRespuesta.appendChild(h2);
+          setTimeout( limpiar,3000);
+            break;
+        case numero > numeroRandom:
+            h2.innerHTML = '<i class="bi bi-x-square-fill"></i> Usted No Adivino el Numero!. Adivino un numero mayor que el Número Mágico';
+            contenedorPadreRespuesta.appendChild(h2);
+            setTimeout( limpiar,3000);
+            break;
+        default:
+            h2.innerHTML = '<i class="bi bi-x-square-fill"></i> Usted No Adivino el Numero!';
+            setTimeout( empezarDeNuevo,3000);
+            break;
     }
 }
 
@@ -36,6 +44,9 @@ function limpiar() {
     } else {
         window.location.reload();
     }
+}
+function empezarDeNuevo(){
+    window.location.reload();
 }
 function generarNumero(min, max) {
     min = Math.ceil(min);
